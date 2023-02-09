ecab1b205b1ef7d15d02c0324523c316218742fb
rodrigocosta77 <rodrigocosta90@live.com>
2023-02-09 13:39:55 -0300

    exercio2

---
+++ src/entradaesaida/exercicio2.java
@@ -0,0 +1,24 @@
+package entradaesaida;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		
+		int totaldias,anos,meses,dias;
+		
+		Scanner leia = new Scanner (System.in);
+		
+		System.out.println("\nEntre com quantos dias você já viveu: ");
+		totaldias = leia.nextInt();//1000
+		
+		anos = totaldias / 365;//2 anos
+		meses = (totaldias % 365)/30;//270 / 30 = 9 meses
+		dias = (totaldias % 365)%30;//270 % 30 = 0 dia
+		
+		System.out.println("\nVocê viveu: "+anos+" ano(s), "+meses+" mes(es) e "+dias+" dia(s) de vida...");
+
+	}
+
+}
